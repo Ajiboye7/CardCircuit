9df4f506ab0ab8abc75c8d693b7a04bee1e6056f
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-07 16:03:22 +0100

    updated

---
+++ component/customButton.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Image  from "next/image";
+
+const customButton = () => {
+  return (
+    <div>
+      <button 
+       type="submit"
+       className=".btn">
+
+      </button>
+    </div>
+  );
+}
+
+export default customButton;

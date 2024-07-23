f9c5f6c45aa2f1f60e091ae735705ecece926f82
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-07-23 21:53:43 +0100

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -123,36 +123,3 @@ export default Features;
     */
 }
 
-/*import Image from "next/image";
-import CustomButton from "./CustomButton";
-
-export default function Section() {
-  return (
-    <section className="container max-w-[1400px] mx-auto p-4">
-      <div className="flex flex-col md:flex-row items-center border-solid border-4 border-light-blue-500">
-        {/* Left Side 
-        <div className="flex-1 md:mr-4 ml-4 md:text-left mb-4 md:mb-0">
-          <h1 className="text-2xl md:text-5xl font-bold mb-4 text-fontColor ">Simplify Your Wallet.<br />Unify Your Finances.</h1>
-        </div>
-        {/* Right Side
-        <div className="flex-1 md:ml-4 md:text-left mb-4 md:mb-0">
-          <p className="text-lg md:text-3xl mb-4 text-fontColor">Welcome to seamless centralized banking. <br/>Manage and track all your cards in one app.</p>
-          <div className="flex justify-center md:justify-start gap-10">
-            <CustomButton title="Get on Android" Icon="/playstore-svgrepo-com 1.svg" />
-            <CustomButton title="Get on iPhone" Icon="/Vector.svg" />
-          </div>
-        </div>
-      </div>
-      {/* Full Width Image 
-      <div className="mt-4">
-        <Image
-          src="/Mockups.svg"
-          alt="analyticSreen"
-          width={500}
-          height={500}
-          className="w-full h-auto"
-        />
-      </div>
-    </section>
-  );
-}*/

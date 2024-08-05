a98dd525fb4febb5c2af4b7a669c77d9e509d283
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-05 07:31:53 +0100

    'update

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <TextWithBackground/>
       <Features/>
       <WithEase/>
-      <TestimonialSection/>
+      {/*<TestimonialSection/>*/}
       <AsFeaturedIn/>
       <GetStarted/>
     </main>

e03ab74b7d709502f330f2163273f14a4d3d904c
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-17 14:49:01 +0100

    'hover'

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,6 +6,46 @@ const Button = ({ title, containerStyles, Icon, textStyles,imageStyle,line1Style
 
   return (
     <div>
+  <button
+    type="submit"
+    className={`custom-btn flex items-center gap-2 bg-buttonBlue ${containerStyles} relative overflow-hidden transition-all duration-500 ease-out group`}
+  >
+    <div className={`absolute inset-0 bg-gradient-to-r from-blue-500 to-green-500 opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-out z-0`}></div>
+    <div className="relative z-10">
+      <div className={textStyles}>
+        {titleLines.map((line, index) =>
+          index === 0 ? (
+            <div key={index} className={line1Styles}>
+              {line}
+            </div>
+          ) : (
+            <div key={index} className={line2Styles}>
+              {line}
+            </div>
+          )
+        )}
+      </div>
+    </div>
+    {Icon && (
+      <div className="relative z-10 flex-shrink-0">
+        <Image
+          src={Icon}
+          alt="icon"
+          width={20}
+          height={20}
+          className={`${imageStyle} transition-transform duration-500 ease-out group-hover:rotate-180`}
+        />
+      </div>
+    )}
+  </button>
+</div>
+
+  );
+};
+
+export default Button;
+{/*
+  <div>
       <button type="submit" className={`custom-btn flex items-center gap-2 bg-buttonBlue ${containerStyles}`}>
         <div className={textStyles}>
          {titleLines.map((line, index) => (
@@ -22,11 +62,7 @@ const Button = ({ title, containerStyles, Icon, textStyles,imageStyle,line1Style
           </div>
         )}
       </button>
-    </div>
-  );
-};
-
-export default Button;
+    </div> */}
 
 
 /*import Image from "next/image";

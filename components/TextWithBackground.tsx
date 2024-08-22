c8918aefe9b1be593e513e47009a256e4fe48c82
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-22 14:58:12 +0100

    'text'

--- components/TextWithBackground.tsx
+++ components/TextWithBackground.tsx
@@ -4,12 +4,12 @@ const TextWithBackground = () => {
   return (
     <section className="w-full">
       <div className="relative flex justify-center items-center overflow-hidden min-h-[100px] sm:min-h-[200px] md:min-h-[200px] lg:min-h-[300px] xl:min-h-[400px] my-16 w-full mx-auto border-y-4 border-BorderLineColour">
-      <h1 className="background-text font-bold absolute text-7xl xl:text-big  text-textBg whitespace-nowrap w-full">
-        Your Cards. Your Rules
-      </h1>
-      <h1 className="foreground-text font-bold relative text-3xl sm:text-5xl md:text-6xl lg:text-7xl text-fontColor">
-        Your Cards. Your Rules
-      </h1>
+      <p className="background-text absolute text-textBg whitespace-nowrap w-full">
+      Your <span className="background-span-text">Cards</span>. Your <span className="background-span-text">Rules</span>
+      </p>
+      <p className="foreground-text relative text-fontColor">
+        Your <span className="foreground-span-text">Cards</span>. Your <span className="foreground-span-text">Rules</span>
+      </p>
     </div>
     </section>
     

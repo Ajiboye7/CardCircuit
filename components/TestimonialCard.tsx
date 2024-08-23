9bece49d6aab7b70f0bcf8ff31703a45359a4f45
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-08-23 14:43:14 +0100

    'change'

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -33,14 +33,15 @@ const TestimonialCard: React.FC<TestimonialProps> = ({ testimonial }) => {
           <h3 className="card-name sm:text-4xl  text-fontColor">
             {testimonial.name}
           </h3>
+          <div className="">
+          <p>I am well</p>
+          </div>
+         
           <p className="card-profession sm:text-2xl mb-1 text-fontColor ">
             {testimonial.profession}
           </p>
           <p className="card-skill text-fontColor">{testimonial.skill}</p>
         </div>
-        <div className="absolute bottom-6 left-1/2 bg-opacity-70 transform -translate-x-1/2 translate-y-0 group-hover:-translate-y-52 transition-transform duration-500 opacity-0 group-hover:opacity-100 ease-in-out w-[350px] sm:w-[400px] h-auto bg-testimonialCardBg p-6 rounded-t-3xl shadow-lg flex flex-col justify-center items-center text-center z-0">
-        <p className="text-1xl sm:text-2xl text-fontColor font-bold">{testimonial.testimony}</p>
-      </div>
       </div>
 
       {/* Pop-up card*/}

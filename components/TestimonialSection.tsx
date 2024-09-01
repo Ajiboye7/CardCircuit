6936a8df133dcd0721b6df8486afa4c1a808ba85
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-01 21:44:12 +0100

    'get'

--- components/TestimonialSection.tsx
+++ components/TestimonialSection.tsx
@@ -20,17 +20,7 @@ const TestimonialSection = () => {
           className='relative w-0 sm:top-[4.5rem] md:top-[7rem] sm:w-[50px] md:w-[100px] lg:w-[120px]'
         />
         <p className='text-fontColor text-center testimonial-title '>
-          What our <span className='text-cardThreeBg font-[800]'>Customers</span> are saying
-        </p>
-      </div>
-
-      <div className="flex gap-10 items-center justify-center flex-wrap py-12 container mx-auto" /*'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto max-w-[1440px]'*/>
-        {Testimonial?.map((testimonial) => (
-          <div key={testimonial.id}>
-            <TestimonialCard testimonial={testimonial}/>
-          </div>
-        ))}
-      </div>
+          What our <span cla
     </div>
   );
 }

ffa6969d9def1c555474c0ab9b613574cacb81a6
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-03 12:33:41 +0100

    'almost'

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -22,8 +22,8 @@ const TestimonialCard: React.FC<TestimonialProps> = ({ testimonial }) => {
       />
 
       {/* Smaller Card */}
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 p-4 bg-testimonialCardBg testimonial-small-card bg-opacity-70 shadow-lg">
-        <div className="flex flex-col gap-3 ">
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 p-4 bg-testimonialCardBg testimonial-small-card bg-opacity-70 shadow-lg rounded-[21.95px] group-hover:rounded-t-none group-hover:rounded-b-[21.95px]">
+        <div className="flex flex-col gap-[0.5rem] xs:gap-3 ">
           <div className="flex items-center mt-5">
             <Image
               src="/Stars.svg"
@@ -45,13 +45,9 @@ const TestimonialCard: React.FC<TestimonialProps> = ({ testimonial }) => {
       </div>
 
       {/* Pop-up card*/}
-      <div className="absolute bottom-3 left-1/2 bg-opacity-70 transform -translate-x-1/2 translate-y-0 group-hover:-translate-y-52 transition-transform duration-500 opacity-0 group-hover:opacity-100 ease-in-out w-[350px] sm:w-[340px] h-auto bg-testimonialCardBg p-6 rounded-t-3xl shadow-lg flex flex-col justify-center items-center text-center z-0">
-        <p className="text-1xl sm:text-2xl text-fontColor font-bold">{testimonial.testimony}</p>
+      <div className="absolute bottom-[0.12rem] xs:bottom-[0.74rem] left-1/2 bg-opacity-70 transform -translate-x-1/2 translate-y-0 group-hover:-translate-y-52 transition-transform duration-700 opacity-0 group-hover:opacity-100 ease-in-out w-[310px] xs:w-[340px] h-auto bg-testimonialCardBg p-6 rounded-t-3xl shadow-lg flex flex-col justify-center items-center text-center z-0">
+        <p className=" pop-card-text text-fontColor font-bold">{testimonial.testimony}</p>
       </div>
-
-      {/*<div className="absolute bottom-6 left-1/2 bg-opacity-70 transform -translate-x-1/2 translate-y-0 group-hover:-translate-y-52 transition-transform duration-1000 opacity-10 group-hover:opacity-100 ease-in-out w-[350px] sm:w-[450px] h-auto bg-testimonialCardBg p-6 rounded-t-3xl shadow-lg flex flex-col justify-center items-center text-center z-0">
-  <p className="text-2xl text-fontColor font-bold">{testimonial.testimony}</p>
-</div>*/}
     </div>
   );
 };
@@ -88,7 +84,7 @@ const TestimonialCard: React.FC<TestimonialProps> = ({ testimonial }) => {
           <p className=" pop-card-text sm:text-2xl text-fontColor font-bold opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-in-out transform -translate-y-full">
             {testimonial.testimony}
           </p>
-          {/* Other Card Content 
+          {/* Other Card Content *
           <div className="flex items-center mt-5">
             <Image
               src="/Stars.svg"
@@ -107,6 +103,14 @@ const TestimonialCard: React.FC<TestimonialProps> = ({ testimonial }) => {
            <p className="card-skill text-center text-fontColor">{testimonial.skill}</p>
         </div>
       </div>
+
+
+    
+
+
+
+
+      
     </div>
   );
 };
